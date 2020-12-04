58dbc061bac100d4bcc78f7dc3bce2d1cbf88670
coolerwind <brzden32@gmail.com>
2020-12-04 11:05:10 +0700

    Finish: add google map api

--- index.js
+++ index.js
@@ -10,17 +10,18 @@ LogBox.ignoreLogs([
     'Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`',
 ])
 
-import Navigation from '/src/configs/Navigation'
-import { Provider } from 'react-redux'
-import store from './src/store'
+// import Navigation from '/src/configs/Navigation'
+// import { Provider } from 'react-redux'
+// import store from './src/store'
 
-function Main() {
-    return (
-        <Provider store={store}>
-            <Navigation />
-        </Provider>
-    )
-}
+// function Main() {
+//     return (
+//         <Provider store={store}>
+//             <Navigation />
+//         </Provider>
+//     )
+// }
+import GoogleMapTest from '/test/GoogleMapTest/googleMapTest'
 // import BottomModalTest from '/test/RefTest/refTest'
 // import DocumentPickerTest from '/test/DocumentPickerTest'
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => GoogleMapTest);
